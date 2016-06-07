0df168e31c206ddfe49fcc460204bc5a61b9b957
kvana <kvanadev1@gmail.com>
2016-06-07 12:04:48 +0530

    Initial Commit

---
+++ Yadadri/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yadadri
+//
+//  Created by Kvana Inc 2 on 07/06/16.
+//  Copyright © 2016 Kvana Inc 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
