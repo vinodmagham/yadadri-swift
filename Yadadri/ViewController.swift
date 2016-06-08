d7b72761023ff7834df57a3eaded53471d997f2b
kvana <kvanadev1@gmail.com>
2016-06-08 10:38:46 +0530

    adding

--- Yadadri/ViewController.swift
+++ Yadadri/ViewController.swift
@@ -19,8 +19,6 @@ class ViewController: UIViewController {
         detailButton.layer.cornerRadius = 5
         detailButton.layer.borderWidth = 1
         detailButton.layer.borderColor = UIColor.blackColor().CGColor
-        
-        
     }
     @IBAction func detailsClicked(sender: AnyObject) {
     
