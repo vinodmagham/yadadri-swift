eba4b38ad4db5598abce9c21500a78dc4e2c049e
saideepdaripally <deepu.sai88@gamil.com>
2016-06-10 19:02:43 +0530

    AudioAlbiumdone

--- Yadadri/SongsTableViewCell.swift
+++ Yadadri/SongsTableViewCell.swift
@@ -8,86 +8,77 @@
 
 import UIKit
 import AVFoundation
-class SongsTableViewCell: UITableViewCell {
+class SongsTableViewCell: UITableViewCell,AVAudioPlayerDelegate{
 
     @IBOutlet weak var musicSlider: UISlider!
     @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var songLable: UILabel!
-   
-    var player:AVAudioPlayer?
+
+    var audioPlayer = AVAudioPlayer()
+    var toggleState = 1
     var timer:NSTimer!
-    
    
     override func awakeFromNib() {
             super.awakeFromNib()
-            musicSlider.value = 0.0
-//        var error : NSError? = nil
-//         let url = NSURL(string: "https://s3.amazonaws.com/kargopolov/BlueCafe.mp3")
-//        playerItem = AVPlayerItem(URL: url!)
-//        player=AVPlayer(playerItem: playerItem!)
+        var updateTimer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
         
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("updateSlider"), userInfo: nil, repeats: true)
+        
+        let pathString = NSBundle.mainBundle().pathForResource("Sreekara Subhakara-(Mr-Jatt.com)", ofType: "mp3")
+        
+        if let pathString = pathString {
+            
+            let pathURL = NSURL(fileURLWithPath: pathString)
+            do {
+                try audioPlayer = AVAudioPlayer(contentsOfURL: pathURL)
+                
+            } catch {
+                print("error")
+            }
+        }
+        musicSlider.maximumValue = Float(audioPlayer.duration)
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
     }
-    func updateMusicSlider(){
-        
-        musicSlider.value = Float(player!.currentTime)
-    }
-  
     
     @IBAction func playButtonTapped(sender: AnyObject) {
-        
-        if player?.rate == 0
-        {
-            player!.stop()
-            playButton.setImage(UIImage(named: "pause"), forState: UIControlState.Normal)
-        } else {
-            music()
-            playButton.setImage(UIImage(named: "play"), forState: UIControlState.Normal)
-//            timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateTime", userInfo: nil, repeats: true)
+        let playBtn = sender as! UIButton
+        if toggleState == 1 {
+            audioPlayer.play()
+            toggleState = 2
+            playBtn.setImage(UIImage(named:"pause.png"),forState:UIControlState.Normal)
             
- 
+        } else {
+            audioPlayer.pause()
+            toggleState = 1
+            playBtn.setImage(UIImage(named:"play.png"),forState:UIControlState.Normal)
         }
         
     }
-    
-    func music(){
-        do
-        {
-        let soundURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Sreekara Subhakara-(Mr-Jatt.com)", ofType: "mp3")!)
-               
-        let error : NSError? = nil
-        player = try AVAudioPlayer(contentsOfURL: soundURL)
-        musicSlider.maximumValue = Float(player!.duration)
-        var timer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: Selector("updateMusicSlider"), userInfo: nil, repeats: true)
-//        player!.delegate = self
-        if error == nil {
-//            player!.delegate = self
-            player!.prepareToPlay()
-            player!.play()
-        }
-        }
-        catch
-        {}
+
+    func updateSlider() {
+        musicSlider.value = Float(audioPlayer.currentTime)
     }
 
-//    func updateTime() {
-//        let cmTime = player!.currentTime
-//        let floatTime = Float(CMTimeGetSeconds(player!.currentTime()))
-//        musicSlider.value = floatTime
-//           timeLabel.text = NSString(format: "%.2f", musicSlider.value) as String
-//    }
-
-    @IBAction func musicSliderAction(sender: AnyObject) {
-        player!.stop()
-        player!.currentTime = NSTimeInterval(musicSlider.value)
-        player!.play()
-        
+    @IBAction func sliderAction(sender: AnyObject) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = NSTimeInterval(musicSlider.value)
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+       
     }
+
+func updateTime() {
+    let currentTime = Int(audioPlayer.currentTime)
+    let duration = Int(audioPlayer.duration)
+    let total = currentTime - duration
+    var totalString = String(total)
+    let minutes = currentTime/60
+    let seconds = currentTime - minutes / 60
+    timeLabel.text = NSString(format: "%02d:%02d", minutes,seconds) as String
+}
     
 }
